680b8797a5cf76fc9932a0517274a6ab43d0208e
tadavarthi-rishi <rtadavar@asu.edu>
2025-08-29 15:44:43 -0400

    demo push 2

--- sample.java
+++ sample.java
@@ -1,1 +1,12 @@
 //sample java file to test git push
+import java.util.Scanner;
+public class HelloWorld
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter your name");
+String name=sc.nextLine();
+System.out.println("Hello "+name);
+}
+}
